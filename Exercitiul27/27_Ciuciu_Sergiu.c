2cbb3ae9f8c178b4c450c6e2b3020339299dfad5
Sergiu <sergiusay06@gmail.com>
2024-04-08 20:51:16 +0300

    comentarii exercitii

---
+++ Exercitiul27/27_Ciuciu_Sergiu.c
@@ -0,0 +1,14 @@
+/*
+Se dau două numere naturale N (0 ≤ N ≤ 105) și K (0 ≤ K ≤ 103) și un șir de N numere naturale
+mai mici decât 103. Să se împartă șirul în exact K subsecvențe astfel încât suma maximelor din
+subsecvențe să fie maximă. Afișați exact K linii reprezentând subsecvențele. Dacă există mai multe
+soluții, se poate afișa oricare. Complexitate maximă O(N log2K + K) timp și O(N+K) memorie.
+
++--------------------+-----------------+
+| kmx.in             |    kmx.out      |
++--------------------+-----------------+
+|   8 3              |   1 5 8         |
+|   1 5 8 4 6 3 2 7  |   4 6 3         |
+|    4               |   2 7           |
++--------------------+-----------------+
+*/
