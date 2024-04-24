bd5711dd7f003f24f1c84318d29e33263bfc942f
Sergiu <sergiusay06@gmail.com>
2024-04-24 18:54:06 +0300

    gata tema 10,16

--- Exercitiul10/10_Ciuciu_Sergiu.c
+++ Exercitiul10/10_Ciuciu_Sergiu.c
@@ -1,14 +1,74 @@
-/*
-Se dă un string de cifre S (1 ≤ S ≤ 1 000). Să se insereze un număr minim de
-paranteze rotunde deschise și închise astfel încât stringul să fie corect
-parantezat și fiecare cifră C să fie în exact C perechi de paranteze. 
-+--------+-----------------+
-| cor.in |    cor.out      |
-+--------+-----------------+
-|   021  |   0((2)1)       |
-|   312  |   (((3))1(2))   |
-|    4   |   ((((4))))     |
-+--------+-----------------+
-*/
+#include <stdio.h>
+int StringLenght(char *String);
+void PunereParanteze(char * string_in, char * string_out);
 
-// idee 1 se face pargurgerea sirului si se introduc datele cu ajutorul stivei.
+int main(){
+	char S_in[1000];
+	char S_out[1000];
+	FILE *inputFile;
+	FILE *outputFile;
+
+	inputFile = fopen("cor.in","r");
+	outputFile = fopen("cor.out","a");
+
+	// verifica ca fisierele sa fie deschise corect
+	if (inputFile == NULL || outputFile == NULL)
+	{
+		printf("eroare la deschiderea fisierelor !!!!");
+		return 0;
+	}
+
+	//loop pentru a aplica algoritmul pe fiecare string din fisier
+	while (fscanf(inputFile,"%s",S_in) != EOF){
+		PunereParanteze(S_in,S_out);
+		fprintf(outputFile,"%s\n",S_out);
+	}
+	
+	fclose(inputFile);
+	fclose(outputFile);
+	return 0;
+}
+
+//lungimea unui string
+int StringLenght(char *String){
+	int i = 0;
+	while (String[i] != '\0')
+		i++;
+	return i;
+	
+}
+
+
+void PunereParanteze(char *String_in, char *String_out) {
+    int i, openParentheses = 0;
+     
+    for (i = 0; i < String_in[0] - '0'; i++)
+        String_out[i] = '(';
+
+    openParentheses += String_in[0] - '0';
+     
+    for (i = 0; i < StringLenght(String_in) - 1; i++) {
+        int diff = String_in[i] - String_in[i + 1];
+        if (diff < 0) {
+             
+            for (int j = 0; j < -diff; j++) {
+                String_out[openParentheses++] = '(';
+            }
+        } else {
+            
+            for (int j = 0; j < diff; j++) {
+                String_out[openParentheses++] = ')';
+            }
+        }
+         
+        String_out[openParentheses++] = String_in[i];
+    }
+     
+    String_out[openParentheses++] = String_in[strlen(String_in) - 1];
+
+    for (i = 0; i < String_in[strlen(String_in) - 1] - '0'; i++) {
+        String_out[openParentheses++] = ')';
+    }
+
+    String_out[openParentheses] = '\0';
+}
--- Exercitiul16/16_Ciuciu_Sergiu.c
+++ Exercitiul16/16_Ciuciu_Sergiu.c
@@ -1,19 +1,82 @@
-/*
-Se dau N cuvinte (N ≤ 10 000), de lungime maximă L caractere (L ≤ 1 000). Pentru fiecare
-cuvânt introdus, creați și afișați pe câte o linie un nou cuvânt format astfel încât să fie:
-- Format prin amestecarea literelor cuvântului original.
-- Mai mare din punct de vedere lexicografic decât cuvântul original.
-- Cel mai mic cuvânt din punct de lexicografic respectând condițiile 1) și 2).
-Dacă cel puțin una dintre cele trei condiții nu poate fi respectată, se va afișa -1 pentru
-cuvântul respectiv. Complexitate maximă O(N × L) timp și O(L) memorie.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-+--------+-----------------+-------------------------------------------------+
-| ams.in |    ams.out      |    Explicații                                   |
-+--------+-----------------+-------------------------------------------------+
-|   4    |                 |    Se citește numărul N, apoi cele N cuvinte,   |
-|  lmno  |      lmon       |    câte un cuvânt pe un rând.                   |
-|  dcba  |       -1        |    Se vor afișa doar cuvintele transformate, în |
-|  abdc  |      acbd       |    ordinea datelor de intrare.                  |
-|  abcd  |      abdc       |                                                 |
-+--------+-----------------+-------------------------------------------------+
-*/
+#define MAXCUVINTE 10000
+#define MAXLITERE 1000
+
+void swap(char *a, char *b);
+void sortare(char * cuvant, int lenght);
+int amestecareCuvant(char* cuvant);
+
+int main(){
+    int NumarCuvinte;
+    char cuvant[MAXLITERE];
+    FILE *inputFile;
+	FILE *outputFile;
+
+	inputFile = fopen("ams.in","r");
+	outputFile = fopen("ams.out","w");
+
+    fscanf(inputFile,"%d",&NumarCuvinte);
+    if (NumarCuvinte > MAXCUVINTE)
+        return 0;
+        
+    
+    for (int i = 0; i < NumarCuvinte; i++){
+        fscanf(inputFile,"%s",cuvant);
+        cuvant[strcspn(cuvant,"\n")] = 0;
+        
+        if(amestecareCuvant(cuvant) == 1)
+            fprintf(outputFile, "%s\n", cuvant);  
+        else 
+            fprintf(outputFile, "-1\n"); 
+    }
+
+    fclose(inputFile);
+    fclose(outputFile);
+
+    return 0;
+}
+
+void swap(char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sortare(char *cuvant ,int length){
+     for (int i = 0; i < length - 1; i++) {
+        for (int j = 0; j < length - i - 1; j++) {
+            if (cuvant[j] > cuvant[j + 1]) {
+                swap(&cuvant[j], &cuvant[j + 1]);
+            }
+        }
+    }
+}
+
+int amestecareCuvant(char* word) {
+    int len = strlen(word);
+
+    int i, j;
+    for (i = len - 2; i >= 0; i--) {
+        if (word[i] < word[i + 1]) {
+            break;
+        }
+    }
+
+    if (i < 0) {
+        return 0;  
+    }
+
+    for (j = len - 1; j > i; j--) {
+        if (word[j] > word[i]) {
+            break;
+        }
+    }
+
+    swap(&word[i], &word[j]);
+    sortare(word + i + 1, len - i - 1);  
+
+    return 1;  
+}
